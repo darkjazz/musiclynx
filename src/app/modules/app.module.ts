414ec45b17197a7dccd1634948d0c5363e9da2e3
alo <alo@anrita.local>
2018-10-09 11:34:31 +0100

    removed obsolete youtube component

--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -12,12 +12,10 @@ import { AppComponent }             from '../components/app.component';
 import { AppRoutingModule, routedComponents } from './routing.module';
 import { ArtistService }            from '../services/artist.service';
 import { MusicBrainzService }       from '../services/musicbrainz.service';
-import { YouTubeService }           from '../services/youtube.service';
 import { DeezerService }            from '../services/deezer.service';
 import { PlayerService }            from '../services/player.service';
 import { ArtistSearchComponent }    from '../components/artist-search.component';
 import { CategoryComponent }        from '../components/category.component';
-import { YouTubeComponent }         from '../components/youtube.component';
 import { AudioComponent }           from '../components/audio.component';
 import { SearchResultsComponent }   from '../components/search-results.component';
 import { Spinner }                  from '../components/spinner.component';
@@ -46,7 +44,6 @@ import { HistoryComponent }         from '../components/history.component';
     AppComponent,
     ArtistSearchComponent,
     CategoryComponent,
-    YouTubeComponent,
     AudioComponent,
     SearchResultsComponent,
     Spinner,
@@ -61,7 +58,6 @@ import { HistoryComponent }         from '../components/history.component';
   providers:    [
     ArtistService,
     MusicBrainzService,
-    YouTubeService,
     DeezerService,
     PlayerService
   ],
